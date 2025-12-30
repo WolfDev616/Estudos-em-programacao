8128a0cc134aff0c4aca504d1ab0ffd4a36f997a
WolfDev616 <andreasamb_@hotmail.com>
2025-12-30 14:12:28 -0300

    Exercício sobre métodos nativos (números)

--- metodosNumeros.js
+++ metodosNumeros.js
@@ -16,8 +16,16 @@
 //150.50
 //200
 // NaN
+ const values = [
+    "200px",
+    "150.50",
+    "abc",
+]
+
+const result = values.map(Number.parseFloat)
 
 
+console.log(result)
 
 
 // Exercício 2: Somar valores numéricos de uma entrada de formulário
@@ -27,8 +35,16 @@
 // Exemplo de Saída:
 // 30.5
 
+let stringInput1 = "";
+let stringInput2 = "";
 
+function sumStringValues(stringInput1, stringInput2) {
+    let sum = ((Number.parseFloat(stringInput1)) + (Number.parseFloat(stringInput2)))
+    console.log(sum);
+    return sum
+}
 
+sumStringValues(49, 53)
 
 // Exercício 3: Arredonde valores financeiros
 // Você tem um valor financeiro com várias casas decimais e deseja exibi-lo com apenas duas
@@ -38,7 +54,10 @@
 // Exemplo de Saída: 
 // Valor formatado: 1234.57
 
+let financialValue = 124535.4959538
 
+console.log(financialValue.toFixed(2))
+console.log(financialValue.toFixed(3))
 
 
 // Exercício 4: Ajustar a precisão de um número
@@ -48,7 +67,8 @@
 // Exemplo de Saída: 
 // Valor com precisão: 1.235e+4
 
-
+console.log(financialValue.toPrecision(2))
+console.log(financialValue.toPrecision(3))
 
 
 // Exercício 5: Verificar se a entrada é um número válido
@@ -59,8 +79,33 @@
 // Exemplo de Saída: 
 // "A entrada não é um número válido."
 
+const entryValues = [
+    "Hello, ms. Jones",
+    "What a fine ass!",
+    "359",
+    "1427.755",
+    "Love you, babe",
+    "666",
+    "Devil's Child"
+]
+
+const entryValuesAnalysis = entryValues
+    .map(Number.parseFloat)
+    // .filter(value => !Number.isNaN(value)); 
 
 
+function checkStringMessage() {  
+
+    for (item of entryValuesAnalysis) {
+        if (Number.isNaN(item)) {
+            console.log("Este item não é um número");}
+        else {
+            console.log("Este item é um número");}
+    }
+}
+    
+console.log(entryValuesAnalysis)
+checkStringMessage()
 
 // Exercício 6: Verificar se o número é finito
 // Você quer garantir que um número não seja Infinity ou NaN antes de usá-lo em cálculos.
@@ -68,3 +113,14 @@
 // NaN ou “abc”;
 // Exemplo de Saída: 
 // ""O número é infinito ou não é um número."
+
+
+function verifyFiniteness(numero) {
+    console.log(Number.isFinite(numero));
+    return
+}
+
+
+verifyFiniteness(numero = 4666);
+verifyFiniteness(numero = "Luci");
+verifyFiniteness(1/0);
